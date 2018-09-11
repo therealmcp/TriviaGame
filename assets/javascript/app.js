ae66bf56fc679bd3dc077acf6a0ba9b2cc288ff9
unknown <cp.mcpike@gmail.com>
2018-09-11 01:05:10 -0700

    so close...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,37 +50,56 @@ function shuffle(array) {
 // Shuffle answers of chosen question
 newAnswers = shuffle(questAnswers);
 
+
+
+var showQuestion = function() {
+    $("#question").text(randQuestion.question);
+    $("#firstChoice").attr("value", newAnswers[0]).text(newAnswers[0]);
+    $("#secondChoice").attr("value", newAnswers[1]).text(newAnswers[1]);
+    $("#thirdChoice").attr("value", newAnswers[2]).text(newAnswers[2]);
+    $("#fourthChoice").attr("value", newAnswers[3]).text(newAnswers[3]);
+    /* for (i = 0; i > newAnswers.length; i++) {
+
+        $(``)
+
+        var button = $(".btn").text()
+        if(newAnswers[i] === randQuestion.answers[0]) {
+            var correctButton = button;
+            console.log(button);
+        }
+    } */
+    timerQ;
+    
+};
+
+// Prevent multiple clicks
+var alreadyClicked = false;
+
 // Add event listener to buttons clicked
 $(".btn").on("click", function() {
     var clicked = $(this).html();
-    if (clicked === randQuestion.answers[0]) {
+    if (clicked === randQuestion.answers[0] && alreadyClicked != true) {
         console.log("correct");
         $(this).addClass("btn-success");
         correctAnswers++;
         $("#correct").text(correctAnswers);
-    } else {
+        alreadyClicked = true;
+    } else if (alreadyClicked != true) {
         $(this).addClass("btn-danger");
-        $(randQuestion.answers[0]).addClass("btn-success"); // Doesn't work
+        var myButtons = $(".btn");
+        console.log(myButtons);
+        for (i = 0; i < myButtons.length; i++) {
+            if (myButtons.attr("value", newAnswers[i]) === randQuestion.answers[0]) {
+                console.log("that is not correct");
+
+            }
+        }
         wrongAnswers++;
         $("#wrong").text(wrongAnswers);
+        alreadyClicked = true;
+
     };
 });
-
-var showQuestion = function() {
-    $("#question").text(randQuestion.question);
-    $("#firstChoice").text(newAnswers[0]);
-    $("#secondChoice").text(newAnswers[1]);
-    $("#thirdChoice").text(newAnswers[2]);
-    $("#fourthChoice").text(newAnswers[3]);
-    for (i = 0; i > newAnswers.length; i++) {
-        if(newAnswers[i] === randQuestion.answers[0]) {
-            
-        }
-    }
-    $(".btn").
-    timerQ;
-    
-};
 
 // Ask question and start timer when the page is loaded
 window.onload = function() {
